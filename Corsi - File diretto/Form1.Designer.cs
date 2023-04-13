96cd0b2d8f6a32e65f52aa949295d124a99c9868
Lucas Corsi <corsi.lucasgabriel.studente@itispaleocapa.it>
2023-04-13 23:29:46 +0200

    Aggiungere i file di progetto.

---
+++ Corsi - File diretto/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Corsi___File_diretto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.salva = new System.Windows.Forms.Button();
+            this.stampa = new System.Windows.Forms.Button();
+            this.cancella = new System.Windows.Forms.Button();
+            this.svuota = new System.Windows.Forms.Button();
+            this.modifica = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // salva
+            // 
+            this.salva.Location = new System.Drawing.Point(51, 233);
+            this.salva.Name = "salva";
+            this.salva.Size = new System.Drawing.Size(75, 23);
+            this.salva.TabIndex = 0;
+            this.salva.Text = "Salva";
+            this.salva.UseVisualStyleBackColor = true;
+            this.salva.Click += new System.EventHandler(this.salva_Click);
+            // 
+            // stampa
+            // 
+            this.stampa.Location = new System.Drawing.Point(160, 233);
+            this.stampa.Name = "stampa";
+            this.stampa.Size = new System.Drawing.Size(75, 23);
+            this.stampa.TabIndex = 1;
+            this.stampa.Text = "Stampa";
+            this.stampa.UseVisualStyleBackColor = true;
+            this.stampa.Click += new System.EventHandler(this.stampa_Click);
+            // 
+            // cancella
+            // 
+            this.cancella.Location = new System.Drawing.Point(51, 284);
+            this.cancella.Name = "cancella";
+            this.cancella.Size = new System.Drawing.Size(75, 23);
+            this.cancella.TabIndex = 2;
+            this.cancella.Text = "Cancella";
+            this.cancella.UseVisualStyleBackColor = true;
+            this.cancella.Click += new System.EventHandler(this.cancella_Click);
+            // 
+            // svuota
+            // 
+            this.svuota.Location = new System.Drawing.Point(51, 415);
+            this.svuota.Name = "svuota";
+            this.svuota.Size = new System.Drawing.Size(102, 23);
+            this.svuota.TabIndex = 3;
+            this.svuota.Text = "Svuota File";
+            this.svuota.UseVisualStyleBackColor = true;
+            this.svuota.Click += new System.EventHandler(this.svuota_Click);
+            // 
+            // modifica
+            // 
+            this.modifica.Location = new System.Drawing.Point(160, 284);
+            this.modifica.Name = "modifica";
+            this.modifica.Size = new System.Drawing.Size(75, 23);
+            this.modifica.TabIndex = 4;
+            this.modifica.Text = "Modifica";
+            this.modifica.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(391, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(397, 426);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 165);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(160, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(285, 165);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.modifica);
+            this.Controls.Add(this.svuota);
+            this.Controls.Add(this.cancella);
+            this.Controls.Add(this.stampa);
+            this.Controls.Add(this.salva);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button salva;
+        private System.Windows.Forms.Button stampa;
+        private System.Windows.Forms.Button cancella;
+        private System.Windows.Forms.Button svuota;
+        private System.Windows.Forms.Button modifica;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Corsi - File diretto/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Corsi___File_diretto
+{
+    public partial class Form1 : Form
+    {
+        public struct prodotto
+        {
+            public string nome;
+            public float prezzo;
+        }
+        string file, appoggio;
+        string line;
+        byte[] br;
+        int recordLength;
+        public prodotto P;
+        int indice;
+        string verità;
+        public Form1()
+        {
+            InitializeComponent();
+            file = @"File.csv";
+            appoggio = @"FileAppoggio.csv";
+            recordLength = 64;
+            indice = 0;
+            verità = "true";
+        }
+
+        private void salva_Click(object sender, EventArgs e)
+        {
+            P.nome = textBox1.Text;
+            P.prezzo = float.Parse(textBox2.Text);
+            scriviAppend(ToString(P, verità), file);
+            indice++;
+        }
+        private void stampa_Click(object sender, EventArgs e)
+        {
+            var f = new FileStream(file, FileMode.Open, FileAccess.ReadWrite);
+            BinaryReader reader = new BinaryReader(f);
+            f.Seek(0, SeekOrigin.Begin);
+            listView1.Clear();
+            while (f.Position < f.Length)
+            {
+                br = reader.ReadBytes(recordLength);
+                line = Encoding.ASCII.GetString(br, 0, br.Length);
+                listView1.Items.Add(line);
+                
+            }
+            f.Close();
+        }
+        private void cancella_Click(object sender, EventArgs e)
+        {
+            var f = new FileStream(file, FileMode.Open, FileAccess.ReadWrite);
+            BinaryReader reader = new BinaryReader(f);
+
+
+
+                f.Seek(0, SeekOrigin.Begin);
+                br = Encoding.ASCII.GetBytes(line);
+                reader.BaseStream.Write(br, 0, br.Length);
+                f.Close();
+        }
+
+        private void svuota_Click(object sender, EventArgs e)
+        {
+            File.Delete(file);
+            listView1.Clear();
+        }
+
+
+        public static string ToString(prodotto P, string verità)
+        {
+            string sep = ";";
+            return (P.nome + sep + P.prezzo + sep + verità).PadRight(60) + "##";
+
+        }
+        public static void scriviAppend(string line, string file)
+        {
+            var oStream = new FileStream(file, FileMode.Append, FileAccess.Write, FileShare.Read);
+            StreamWriter sw = new StreamWriter(oStream);
+            sw.WriteLine(line);
+            sw.Close();
+        }
+        public static string cercaSuFile(string file)
+        {
+            StreamReader sr = new StreamReader(file);
+            string line = "";
+             
+            while (!sr.EndOfStream)
+            {
+               
+
+
+            }
+
+            sr.Close();
+
+            return line;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
