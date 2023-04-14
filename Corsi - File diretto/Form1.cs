daf99054e943710d1958dfa1e63e8d10d0018383
Corsi Lucas <corsi.lucasgabriel.studente@itispaleocapa.it>
2023-04-14 13:55:08 +0200

    cancellazione logica forse

--- Corsi - File diretto/Form1.cs
+++ Corsi - File diretto/Form1.cs
@@ -18,28 +18,28 @@ namespace Corsi___File_diretto
             public string nome;
             public float prezzo;
         }
-        string file, appoggio;
+        string file;
         string line;
         byte[] br;
         int recordLength;
-        public prodotto P;
+        public prodotto[] P;
         int indice;
-        string verità;
+        int linea;
         public Form1()
         {
             InitializeComponent();
             file = @"File.csv";
-            appoggio = @"FileAppoggio.csv";
             recordLength = 64;
             indice = 0;
-            verità = "true";
+            linea = 0;
+            P = new prodotto[100];
         }
 
         private void salva_Click(object sender, EventArgs e)
         {
-            P.nome = textBox1.Text;
-            P.prezzo = float.Parse(textBox2.Text);
-            scriviAppend(ToString(P, verità), file);
+            P[indice].nome = textBox1.Text;
+            P[indice].prezzo = float.Parse(textBox2.Text);
+            scriviAppend(ToString(P, indice), file);
             indice++;
         }
         private void stampa_Click(object sender, EventArgs e)
@@ -59,15 +59,15 @@ namespace Corsi___File_diretto
         }
         private void cancella_Click(object sender, EventArgs e)
         {
+            string sep = ";";
+            cercaSuFile(file, textBox1.Text, ref linea);
             var f = new FileStream(file, FileMode.Open, FileAccess.ReadWrite);
             BinaryReader reader = new BinaryReader(f);
-
-
-
-                f.Seek(0, SeekOrigin.Begin);
-                br = Encoding.ASCII.GetBytes(line);
-                reader.BaseStream.Write(br, 0, br.Length);
-                f.Close();
+            f.Seek(recordLength * linea, SeekOrigin.Begin);
+            line = Encoding.ASCII.GetString(br, 0, br.Length);
+            br = Encoding.ASCII.GetBytes((P[linea].nome + sep + P[linea].prezzo + sep + "false" + sep).PadRight(60) + "##");
+            reader.BaseStream.Write(br, 0, br.Length);
+            f.Close();
         }
 
         private void svuota_Click(object sender, EventArgs e)
@@ -77,10 +77,10 @@ namespace Corsi___File_diretto
         }
 
 
-        public static string ToString(prodotto P, string verità)
+        public static string ToString(prodotto[] P, int indice)
         {
             string sep = ";";
-            return (P.nome + sep + P.prezzo + sep + verità).PadRight(60) + "##";
+            return (P[indice].nome + sep + P[indice].prezzo + sep + "true" + sep).PadRight(60) + "##";
 
         }
         public static void scriviAppend(string line, string file)
@@ -90,21 +90,27 @@ namespace Corsi___File_diretto
             sw.WriteLine(line);
             sw.Close();
         }
-        public static string cercaSuFile(string file)
+        public static int cercaSuFile(string file, string input, ref int linea)
         {
             StreamReader sr = new StreamReader(file);
             string line = "";
-             
+            linea = 0;
+
             while (!sr.EndOfStream)
             {
-               
-
-
+                line = sr.ReadLine();
+                String[] split = line.Split(';');
+                
+                if(split[0] == input)
+                {
+                    break;
+                }
+                linea++;
             }
 
             sr.Close();
 
-            return line;
+            return linea;
         }
 
         private void Form1_Load(object sender, EventArgs e)
